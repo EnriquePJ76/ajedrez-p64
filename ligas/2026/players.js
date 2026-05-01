4b69d1a4d14ed9a3dffa1d3a784220e0bd9ed129
Enrique PJ <enrique@ridan-v.systeca.local>
2026-05-01 17:33:41 +0200

    Liga 2026: mostrando jugadores

--- ligas/2026/players.js
+++ ligas/2026/players.js
@@ -1,4 +1,12 @@
 
+function show_player_list(players) {
+   const olPlayers = document.getElementById('players');
+
+   Object.keys(players).forEach(key => {
+      console.log(key + ' => ' + players[key]);
+   })
+}
+
 const requestURL = 'players.json';
 const request = new XMLHttpRequest();
 
@@ -8,6 +16,7 @@ request.send();
 
 request.onload = function () {
    const players = request.response;
-   console.log(players);
+   show_player_list(players);
 }
+
 
